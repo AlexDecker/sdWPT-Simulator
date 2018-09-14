1a4a9e8801ab3afa0f69ee5286359dbcb0533649
AlexDecker <alexanderdecker@dcc.ufmg.br>
2018-09-14 15:21:30 -0300

    Qi tunning

--- SIMULATOR_main/STEIN_tunning.m
+++ SIMULATOR_main/STEIN_tunning.m
@@ -2,12 +2,12 @@ clear all;
 
 W = 2*pi*linspace(110000,205000,3);%110-205kHz
 R1 = 0.025*[1,2,3,4,5];
-R2 = [10,30,40,50];
+R2 = 30;
 C1 = linspace(100e-9,400e-9,4);
 C2 = 183e-9*[0.5,1,1.5,2];%a capacitância paralela é desconsiderada por ser muito baixa e, portanto, gerar uma ponte de alta impedância
-ZONE1 = [0.012,0.014];
-ZONE2 = [0.0145,0.016];
-MIENV1 = linspace(pi*4e-7,pi*16e-7,6);%permissividade magnética do meio nas proximidades do atrator
+ZONE1 = 0.014;
+ZONE2 = 0.016;
+MIENV1 = linspace(pi*4e-7,pi*20e-7,6);%permissividade magnética do meio nas proximidades do atrator
 MIENV2 = pi*4e-7;
 
 %valor de referência
@@ -16,13 +16,13 @@ ref_dist = [7.50, 10.00, 12.5, 15.0, 17.5, 20.0, 22.5];
 
 data = [];
 
-hold on
-plot(ref_dist,ref_eff,'r');
+%hold on
+%plot(ref_dist,ref_eff,'r');
 num=0;
-for c1 = C1
-	for c2 = C2
-		for r1 = R1
-			for r2 = R2
+for r2 = R2
+	for r1 = R1
+		for c1 = C1
+			for c2 = C2
 				for w = W
 					for miEnv1 = MIENV1
 						for miEnv2 = MIENV2
@@ -35,14 +35,14 @@ for c1 = C1
 									params.zone2Limit = z2;
 									params.miEnv1 = miEnv1;
 									params.miEnv2 = miEnv2;
-									[t_TX, BC_TX1, BC_TX2, t_RX, CC_RX] = simulate_STEIN();%params);
-									convertendo tempo em distância
+									[t_TX, BC_TX1, BC_TX2, t_RX, CC_RX] = simulate_STEIN(params);
+									%convertendo tempo em distância
 									d_TX = ((1000-t_TX)*5 + 30*t_TX)/1000;
 									d_RX = ((1000-t_RX)*5 + 30*t_RX)/1000;
 						
 									err = 0;
 									eff_list = [];
-									for i=1:length(ref_eff)
+									for i=1:3%length(ref_eff)
 										i_tx1 = interp1(d_TX,BC_TX1,ref_dist(i));
 										i_tx2 = interp1(d_TX,BC_TX2,ref_dist(i));
 										i_rx = interp1(d_RX,CC_RX,ref_dist(i));
@@ -73,7 +73,7 @@ for c1 = C1
 									num=num+1;
 									disp(['progresso: ',num2str(100*num/(length(C1)*length(C2)*length(R1)*length(R2)*length(W)*length(MIENV1)*length(MIENV2)*length(ZONE1)*length(ZONE2))),'%']);
 									data=[data,d];
-									plot(ref_dist,eff_list,'b');
+									%plot(ref_dist,eff_list,'b');
 								end
 							end
 						end
