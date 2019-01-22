ee3e9c30fc3a8aa3a9c97352fde7a3b84d689c1b
AlexDecker <alexanderdecker@dcc.ufmg.br>
2019-01-22 16:51:34 -0200

    oriented tunning

--- tunning_deletar/tunningMagMIMO.m
+++ tunning_deletar/tunningMagMIMO.m
@@ -24,139 +24,164 @@ rlLookupTable = LookupTable('magMIMOLinearBattery_data.txt',false);
 %tensão em função da carga
 vbLookupTable = LookupTable('Li_Ion_Battery_LIR18650.txt',false);
 
-hold on;
+%hold on;
 E = [];%dados de log
 minErr = inf;%erro mínimo conhecido até o momento
 
-for d=1:length(data)%instância (design da bobina receptora)
+%vetor de probabilidades não normalizado, para a escolha da instância de ambiente
+prob = ones(length(data),1);
+
+%melhores resultados para cada instância de ambiente
+bests = inf*ones(length(data),1);
+
+%rr que gerou o melhor resultado
+rrbests = 0.05*ones(length(data),1);
+
+%rt que gerou o melhor resultado
+rtbests = 30*ones(length(data),1);
+
+while(true)
+	%escolhendo uma instância a ser utilizada
+	d = sum(rand>=cumsum(prob/sum(prob)))+1;
 	%para o controle das instâncias a serem utilizadas
-	if((data(d).N_rx<21)&&(data(d).N_rx>20)&&(data(d).A_rx<0.075)&&(data(d).B_rx<0.075))
-	%if((data(d).B_rx<0.051863)&&(data(d).B_rx>0.051859)&&(data(d).R1_rx>0.055362)&&(data(d).R1_rx<0.055366))
+	if((data(d).N_rx>1)&&(data(d).A_rx<0.075)&&(data(d).B_rx<0.075))
+		%escolhendo os parâmetros
+		Rr = 0;
+		while(Rr<=0)
+			Rr = rrbests(d)+(rand-0.5)*min(bests(d),1)/10;
+		end
+		Rt = 0;
+		while(Rt<=0)
+			Rt = rtbests(d)+(rand-0.5)*min(bests(d),1);
+		end
 		disp(['Iniciando nova instância: R1_rx=',num2str(data(d).R1_rx),...
 			' N_rx=',num2str(data(d).N_rx),...
 			' A_rx=',num2str(data(d).A_rx),...
-			' B_rx=',num2str(data(d).B_rx)]);
-		for Rr = 30%linspace(25,40,4)%[2,5,10,20,25,30]%resistência do ressonador no receptor
-		%resistência de cada ressonador transmissor
-			for Rt = 1.667%linspace(1.5,1.8,4)%[0.01,0.025,0.05,0.075,0.1,0.25,0.5]
-				err = [0,0,0,0];%erro (para cada distância)
-				%tempos de término
-				ft = [];
-				for i=1:4%para cada distância
-					%calculando a matriz de impedância
-					Z = -(1i)*w*u*data(d).M(i).obj + diag([Rt*ones(6,1);Rr]);
-					%vetor de qualidade de canal*Rl
-					m_Rl = (1i)*w*u*data(d).M(i).obj(1:6,7);
-					%lista com a progressão da carga
-					Q = [];
-					%lista com a progressão da resistência equivalente do circuito da bateria
-					RL = [];
-					%lista com a progressão da corrente no anel receptor
-					IL = [];
-					%carga atual (inicialmente morta)
-					q=0;
-					%tempo máximo de simulação em horas
-					ttl=20;
-					%tempo desde o início dessa simulação
-					t = 0.4/3600;
-					%contagem de tempo de simulação
-					timeCount = 0;
-					fase=0;
-					while true
-						%obtem a resistência equivalente do circuito da bateria
-						%(dado o estado atual)
-						Rl = getYFromX(rlLookupTable,q/Qmax);
-						RL = [RL;Rl];
-						%obtém a tensão da bateria dado o estado atual
-						Vb = getYFromX(vbLookupTable,q/Qmax);
-						%vetor de qualidade de canal
-						m  = m_Rl/(Rl+Rr);
-						%fazendo o tempo passar
-						t = t-step;
-						if fase<6
-							I = (Z+diag([zeros(6,1);Rl]))\[zeros(fase,1);1;zeros(6-fase,1)];
-							Il = I(7);
-							if t<=0
-								fase = fase+1;
-								if fase==6
-									t = 30/3600;
-								else
-									t = 0.4/3600;
-								end
-							end
+			' B_rx=',num2str(data(d).B_rx),...
+			' Rr_rx=',num2str(Rr),...
+			' Rr_tx=',num2str(Rt)]);
+		
+		err = [0,0,0,0];%erro (para cada distância)
+		%tempos de término
+		for i=1:4%para cada distância
+			%calculando a matriz de impedância
+			Z = -(1i)*w*u*data(d).M(i).obj + diag([Rt*ones(6,1);Rr]);
+			%vetor de qualidade de canal*Rl
+			m_Rl = (1i)*w*u*data(d).M(i).obj(1:6,7);
+			%lista com a progressão da carga
+			Q = [];
+			%lista com a progressão da resistência equivalente do circuito da bateria
+			RL = [];
+			%lista com a progressão da corrente no anel receptor
+			IL = [];
+			%carga atual (inicialmente morta)
+			q=0;
+			%tempo máximo de simulação em horas
+			ttl=10;
+			%tempo desde o início dessa simulação
+			t = 0.4/3600;
+			fase=0;
+			while true
+				%obtem a resistência equivalente do circuito da bateria
+				%(dado o estado atual)
+				Rl = getYFromX(rlLookupTable,q/Qmax);
+				RL = [RL;Rl];
+				%obtém a tensão da bateria dado o estado atual
+				Vb = getYFromX(vbLookupTable,q/Qmax);
+				%vetor de qualidade de canal
+				m  = m_Rl/(Rl+Rr);
+				%fazendo o tempo passar
+				t = t-step;
+				if fase<6
+					I = (Z+diag([zeros(6,1);Rl]))\[zeros(fase,1);1;zeros(6-fase,1)];
+					Il = I(7);
+					if t<=0
+						fase = fase+1;
+						if fase==6
+							t = 30/3600;
 						else
-							%calculando a base do vetor de correntes do dispositivo transmissor
-							beta = m./sum(abs(m).^2);
-							%calculando a corrente do receptor caso seja aplicada beta
-							bl = m.'*beta;
-							%normalizando a corrente de transmissão baseada em m mas de forma a
-							%não superar a potência P
-							It = beta*...
-								(sqrt(P/real([beta;bl]'*(Z+diag([zeros(6,1);Rl]))*[beta;bl])));
-							%aplicando a nova corrente transmissora e obtendo a corrente de
-							%recebimento
-							Il = m.'*It;
-							if t<=0
-								fase = 0;
-								t = 0.4/3600;
-							end
+							t = 0.4/3600;
 						end
-						IL = [IL;Il];
-						%corrente de descarga
-						if Vb>=minVTO
-							Id = sPower/Vb;
-						else
-							Id = 0;
-						end
-						%incrementando a carga
-						q = q+eff*(abs(Il)-Id)*step;
-						%limitando a carga a 100%
-						if q>=Qmax
-							Q = [Q;Qmax];
-							break;
-						else
-							Q = [Q;q];
-						end
-						%controle de tempo
-						ttl = ttl-step;
-						if ttl<0
-							break;
-						end
-						t = t-step;
-						timeCount = timeCount+step;
 					end
-					%se o loop foi rompido pelo ttl<0, o teste foi muito desastroso para ser
-					%considerado
-					if ttl<0
-						err(i) = inf;
-					else
-						%calculando o erro em relação ao referencial adotado
-						err_parc = (Q.')/Qmax-coef_ref(i)...
-							*linspace(0,step*(length(Q)-1),length(Q));
-						err(i) = mean(err_parc.^2);
+				else
+					%calculando a base do vetor de correntes do dispositivo transmissor
+					beta = m./sum(abs(m).^2);
+					%calculando a corrente do receptor caso seja aplicada beta
+					bl = m.'*beta;
+					%normalizando a corrente de transmissão baseada em m mas de forma a
+					%não superar a potência P
+					It = beta*...
+						(sqrt(P/real([beta;bl]'*(Z+diag([zeros(6,1);Rl]))*[beta;bl])));
+					%aplicando a nova corrente transmissora e obtendo a corrente de
+					%recebimento
+					Il = m.'*It;
+					if t<=0
+						fase = 0;
+						t = 0.4/3600;
 					end
-					ft = [ft, timeCount];
-					%plot(cFactor(i)*linspace(0,step*(length(Q)-1),length(Q)),100*Q/Qmax);
-					%plot(linspace(0,step*(length(RL)-1),length(RL)),RL);
-					plot(linspace(0,step*(length(Q)-1),length(Q)),100*Q/Qmax);
-					plot(linspace(0,1/coef_ref(i),2),100*coef_ref(i)*linspace(0,1/coef_ref(i),2),'g');
 				end
-				%log
-				e.Rt = Rt;
-				e.Rr = Rr;
-				e.err = sum(err);
-				e.d = d;
-				E = [E,e];
-				%verificando o menor valor de erro
-				if e.err<minErr
-					minErr = e.err;
+				IL = [IL;Il];
+				%corrente de descarga
+				if Vb>=minVTO
+					Id = sPower/Vb;
+				else
+					Id = 0;
 				end
-				disp(['MIN ERR: ', num2str(minErr)]);
-				if ~isempty(ft)
-					disp((ft-ft(1))*(1/coef_ref(end)-1/coef_ref(1))/(ft(end)-ft(1))+1/coef_ref(1));
+				%incrementando a carga
+				q = q+eff*(abs(Il)-Id)*step;
+				%q/Qmax%DISP
+				%limitando a carga a 100%
+				if q>=Qmax
+					Q = [Q;Qmax];
+					break;
+				else
+					Q = [Q;q];
 				end
+				%controle de tempo
+				ttl = ttl-step;
+				if ttl<0
+					break;
+				end
+				t = t-step;
+			end
+			%se o loop foi rompido pelo ttl<0, o teste foi muito desastroso para ser
+			%considerado
+			if ttl<0
+				err(i) = inf;
+			else
+				%calculando o erro em relação ao referencial adotado
+				err_parc = (Q.')/Qmax-coef_ref(i)...
+					*linspace(0,step*(length(Q)-1),length(Q));
+				err(i) = mean(err_parc.^2);
+			end
+			disp('ok');
+			%plot(cFactor(i)*linspace(0,step*(length(Q)-1),length(Q)),100*Q/Qmax);
+			%plot(linspace(0,step*(length(RL)-1),length(RL)),RL);
+			%plot(linspace(0,step*(length(Q)-1),length(Q)),100*Q/Qmax);
+			%plot(linspace(0,1/coef_ref(i),2),100*coef_ref(i)*linspace(0,1/coef_ref(i),2),'g');
+		end
+		%log
+		e.Rt = Rt;
+		e.Rr = Rr;
+		e.err = sum(err);
+		e.d = d;
+		E = [E,e];
+		%atualizando o vetor de 'probabilidades'
+		prob(d) = prob(d)*max(min((minErr/e.err),1),0)^0.1;
+		if(prob(d)<=0)
+			prob = prob+prob(d)+0.1;
+		end
+		%verificando se melhorou para essa instância
+		if e.err<bests(d)
+			rrbest(d) = Rr;
+			rtbest(d) = Rt;
+			bests(d) = e.err;
+			%verificando o menor valor de erro global
+			if e.err<minErr
+				minErr = e.err;
 			end
 		end
+		disp(['MIN ERR: ', num2str(minErr)]);
 	end
 end
 
